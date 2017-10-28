f3a4e4876d1108b02c509f050e1a055736b79fc9
bluishwhite <2876306424@qq.com>
2017-10-28 23:51:36 +0800

    Add files via upload

---
+++ JS_TASK/upper.js
@@ -0,0 +1,20 @@
+var out=document.getElementById("out");
+var mask=document.getElementsByClassName("mask");
+var modal_flow = document.getElementsByClassName("modal_flow");
+var confirm=document.getElementById("confirm");
+var cancel=document.getElementById("cancel");
+
+
+out.onclick=function show(){
+  modal_flow[0].style.display='block';
+  mask[0].style.display='block';
+}
+
+function hide(){
+  modal_flow[0].style.display='none';
+  mask[0].style.display='none';
+}
+confirm.onclick=hide;
+cancel.onclick=hide;
+
+
